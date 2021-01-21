5be5fc5b5f2a10960ba1e0d4f1fa52f45d883f68
Pcat0 <patrick.ambli@gmail.com>
2021-01-21 05:50:24 -0600

    Start of swipe controls

--- modules/Vector.js
+++ modules/Vector.js
@@ -1,7 +1,16 @@
 export class Vector2 { //Why am I implementing vector math for a shit post?
     constructor(x, y){
-        this.x = x;
-        this.y = y;
+        if (x != null && x.constructor === Object && x.hasOwnProperty('x') && x.hasOwnProperty('y')){
+            this.x = x.x;
+            this.y = x.y;
+        } else {
+            this.x = x;
+            this.y = y;
+        }
+        
+    }
+    get magnitude () {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
     *[Symbol.iterator](){
         yield this.x;
@@ -20,4 +29,5 @@ export class Vector2 { //Why am I implementing vector math for a shit post?
         return new Vector2(this.x / scaler, this.y / scaler);
     }
     static get ZERO(){return new Vector2(0,0)}
+    static get NULL(){return new Vector2(null,null)}
 }
